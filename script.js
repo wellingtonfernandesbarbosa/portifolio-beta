547d3e8088f9eec15ea7578769a4f85bfef4e8b9
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-03-13 23:23:52 -0300

    Refatoração e adição de imagens aos links

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function mudaTema() {
      document.body.classList.toggle("dark");
 }
+
+const botao = document.querySelector("#tema");
+
+botao.addEventListener('click', function() {
+     mudaTema();
+})
