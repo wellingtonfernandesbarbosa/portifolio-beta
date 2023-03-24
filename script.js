c342678e22554ebb150e2a5f9e96fa20583b388f
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2023-03-24 10:49:54 -0300

    Alternancia de tema
    Adicionado a alternancia de tema ao trocar o tema do dispositivo.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 function mudaTema() {
-     document.body.classList.toggle("dark");
-}
+  document.body.classList.toggle("dark");
+};
 
 const botao = document.querySelector("#tema");
-
-botao.addEventListener('click', function() {
-     mudaTema();
-});
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     mudaTema();
 };
+
+botao.addEventListener("click", function () {
+  mudaTema();
+});
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+    const isDarkMode = e.matches;
+    if (isDarkMode) {
+        mudaTema();
+    } else {
+        mudaTema();
+    }
+});
