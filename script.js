bf0318e1e8799183424af3d9b8f950bc914622f2
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-03-23 21:56:51 -0300

    Responsividade, acessibilidade e desempenho

--- script.js
+++ script.js
@@ -6,4 +6,8 @@ const botao = document.querySelector("#tema");
 
 botao.addEventListener('click', function() {
      mudaTema();
-})
+});
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    mudaTema();
+};
